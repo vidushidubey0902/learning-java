5f27c7729ab2198f541ee095b0f23bf970fc6bf1
vidushidubey0902 <116879428+vidushidubey0902@users.noreply.github.com>
2024-02-27 19:21:50 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+// q1 print welcome message
+
+class welcome
+{
+	public static void main(String args[])
+	{
+		System.out.println("welcome screen...first java program");
+	}
+}
---
+++ twoclass.java
@@ -0,0 +1,16 @@
+//q2 creating multiple classes in a single java file....try executing both classes
+
+public class class1
+{	public static void main(STring args[])
+	{
+		System.out.println("class 1 executed");
+	}
+}
+
+public class class2
+{
+	public static void main(STring args[])
+	{
+		System.out.println("class 2 executed");
+	}
+}
